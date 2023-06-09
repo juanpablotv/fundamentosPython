c85af73624a727dd077f86209667578ca7529622
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-06-09 16:52:45 -0600

    04 operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# operadores matematicos
+# + - * %
+# / divicion float
+# // divicion entera
+# ** potencia
+ 
+print(12 * 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+# operadores para los strings
+# + para concatenar
+# * para replicar
+print("_____" * 4)
+print("hola " + " mundo")
+print("hola " * 3)
