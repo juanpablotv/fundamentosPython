d8c82fe61f469914672f3f56a69da507820abf22
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-06-09 17:02:02 -0600

    05 operadores booleanos y logicos

---
+++ operadores_2.py
@@ -0,0 +1,18 @@
+# operadores booleanos 
+# ==(igualdad)
+print( 1 == 1)
+# != (distinto)
+print(1!= 1)
+# > (mayor que)
+print(1 > 1)
+# < (menor que)
+print(1 < 1)
+# operadores logicos
+# and
+print(True and True)
+print(True and False )
+# or
+print(True or True)
+print(False or False)
+# not 
+print( not True)
