fbd57c762146a6f9d4f031c5d5cb935d58cef05c
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-06-13 13:01:56 -0600

    07 strings

---
+++ strings.py
@@ -0,0 +1,26 @@
+my_string = "esTe eS Mi StrInG"
+
+# longitud de string
+print(len (my_string))
+# mayusculas
+print(my_string.upper())
+# minusculas 
+print(my_string.lower())
+# capitalice 
+print(my_string.capitalize())
+# eliminar espacios inicio fin
+print(my_string.strip())
+
+# index and slicing
+my_string = "hola 3C DSM"
+print(my_string [0])
+print(my_string [6])
+print(my_string [3])
+print(my_string [-1])
+print(my_string [-2])
+    
+print(my_string [0:2])
+print(my_string [5:7])
+print(my_string [5:-1])
+
+
