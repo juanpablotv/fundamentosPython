4364fbf3370405e5b1a4def8f3e9a352323abe3a
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-06-15 10:25:40 -0600

    08 listas - crud

---
+++ crud.py
@@ -0,0 +1,34 @@
+drinks = []
+
+def add_drink(drink):
+    drinks.append(drink)
+
+def del_drink(drink):
+    drinks.remove(drink)
+
+def show_drinks():
+    print("-" * 4, "my drinks", "-" * 4)
+    for drink in drinks:
+        print("-->", drink)    
+
+chise_text = """
+elige una opcion:
+1 - agregar
+2 - eliminar
+3 - mostrar
+4 - salir
+"""   
+while  True:
+    choise_user = int(input(chise_text))
+    if choise_user == 1:
+        drink = input('ingresa una bebida: ')
+        add_drink(drink)
+    elif choise_user == 2:
+        drink = input('ingresa una bebida: ')
+        del_drink(drink)
+    elif choise_user == 3:
+        show_drinks()
+    elif choise_user == 4:
+        break
+    else:
+        print('opción incorrecta')
